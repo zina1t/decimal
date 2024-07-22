c91f7ea787773726dd7e4d550008e2fb2ef1fa8a
zina1t <79532105+zina1t@users.noreply.github.com>
2024-07-22 09:40:55 +0000

    s21_add to file

--- src/s21_add.c
+++ src/s21_add.c
@@ -1,22 +1,56 @@
 #include "s21_decimal.h"
 
-/*
 int s21_add(s21_decimal num1, s21_decimal num2, s21_decimal* res) {
-    int carry = 0;
-    for (int i = 0; i < 4; i++) {
-        unsigned int sum = num1.bits[i] + num2.bits[i] + carry;
-        carry = sum >> 31;
-        res->bits[i] = sum;
-    }
-    return carry;
-}
+    s21_decimal temp1 = num1;
+    s21_decimal temp2 = num2;
 
-int s21_add_help(s21_decimal num1, s21_decimal num2, s21_decimal* res) {
-    s21_big_decimal big_num1;
-    s21_big_decimal big_num2;
-    int scale1 = s21_get_scale(num1);
-    int scale2 = s21_get_scale(num2);
-    s21_big_normalize(num1, num2, &big_num1, &big_num2);
-    s21_big_decimal result = s21_big_binary_add(big_num1, big_num2);
+    // Align scales of both numbers
+    align_scales(&temp1, &temp2);
+
+    // Initialize result
+    s21_decimal result = {0};
+    int carry = 0;
+
+    // Add bit by bit
+    for (int i = 0; i < 96; i++) {
+        int bit1 = s21_get_bit(temp1, i);
+        int bit2 = s21_get_bit(temp2, i);
+        int sum = bit1 + bit2 + carry;
+
+        if (sum >= 2) {
+            carry = 1;
+            sum -= 2;
+        } else {
+            carry = 0;
+        }
+
+        s21_set_bit(&result, sum, i);
+    }
+
+    // Check for overflow
+    if (carry != 0) {
+        return 1; // Overflow occurred
+    }
+
+    // Set the scale of the result
+    int scale = s21_get_scale(temp1);
+    s21_set_scale(&result, scale);
+
+    // Set the sign of the result
+    int sign1 = s21_get_sign(temp1);
+    int sign2 = s21_get_sign(temp2);
+
+    if (sign1 == sign2) {
+        s21_set_sign(&result, sign1);
+    } else {
+        // Handle mixed signs (considering larger magnitude for sign determination)
+        if (s21_get_last_bit(temp1) > s21_get_last_bit(temp2)) {
+            s21_set_sign(&result, sign1);
+        } else {
+            s21_set_sign(&result, sign2);
+        }
+    }
+
+    *res = result;
+    return 0; // Successful addition
 }
-*/
