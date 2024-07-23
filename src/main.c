ffca994426b1273063a598acd03c2374d4ac25eb
zina1t <79532105+zina1t@users.noreply.github.com>
2024-07-23 18:56:29 +0000

    removed extra files

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 
-#include "binary.h"
+#include "s21_decimal.h"
 int main(void) {
     s21_decimal decimal1 = {{0x1, 0x0, 0x0, 0x0}};
     s21_decimal decimal2 = {{0x9999999A, 0x99999999, 0x19999999, 0x80000000}};
     s21_decimal result = {{0, 0, 0, 0}};
-    s21_add(decimal1, decimal2, &result);
-    printf("error%d\n", s21_add(decimal1, decimal2, &result));
+    s21_sub(decimal1, decimal2, &result);
+    printf("error%d\n", s21_sub(decimal1, decimal2, &result));
     for (int i = 0; i < 4; i++) {
         printf("%u\n", result.bits[i]);
     }
